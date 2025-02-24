ba0d24ea02230bbd806a30f4de1d1b789bebc99a
Amr-Elharery <amrelharery@gmail.com>
2025-02-24 21:54:14 +0200

    Setup database connection

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+  namespace App\Database;
+  class Database{
+    private static $connection = null;
+
+    public static function connect(){
+      $ENV = include __DIR__ . 'env.php';
+      if(self::$connection == null){
+        $connection = new mysqli($ENV['SERVER_NAME'], $ENV['USER_NAME'], $ENV['PASSWORD'], $ENV['DB_NAME']);
+      }
+      return self::$connection;
+    }
+  }
+?>
