60800f573ecb5a01eda9f28274676dcf48b468b7
Amr-Elharery <amrelharery@gmail.com>
2025-02-25 20:06:25 +0200

    Remove namespaces & Fix DB config

--- config/db.php
+++ config/db.php
@@ -1,10 +1,9 @@
 <?php
-  namespace App\Database;
   class Database{
     private static $connection = null;
 
     public static function connect(){
-      $ENV = include __DIR__ . 'env.php';
+      $ENV = include __DIR__ . '/env.php';
       if(self::$connection == null){
         $connection = new mysqli($ENV['SERVER_NAME'], $ENV['USER_NAME'], $ENV['PASSWORD'], $ENV['DB_NAME']);
       }
--- public/index.php
+++ public/index.php
@@ -1,3 +1,15 @@
 <?php
-  echo '<h1>Hello mystore!</h1>'
+  echo '<h1>Hello mystore!</h1>';
+  
+  include __DIR__ . '/../config/db.php';
+  $database = new Database();
+  try{
+      $connection = $database->connect();
+    if($connection){
+      echo 'Connected to the database';
+    }
+  }
+  catch(Exception $e){
+    echo 'Caught exception: ', $e->getMessage(), "\n";
+  }
 ?>
