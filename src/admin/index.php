398961cd20ba72f28dbd4c89553131e89b8cea19
Amr-Elharery <amrelharery@gmail.com>
2025-02-26 22:37:07 +0200

    Add admin panel

---
+++ src/admin/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel</title>
+  </head>
+  <body>
+    <?php
+      include __DIR__ . '/layout/header.php';
+      include __DIR__ . '/pages/home.php';
+      include __DIR__ . '/layout/footer.php';
+    ?>
+  </body>
+</html>
