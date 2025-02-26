cd261d495f35711f87ccd1e15e3be703e64d9a1b
Amr-Elharery <amrelharery@gmail.com>
2025-02-26 22:36:44 +0200

    Fix index file

--- public/index.php
+++ public/index.php
@@ -1,15 +1,16 @@
-<?php
-  echo '<h1>Hello mystore!</h1>';
-  
-  include __DIR__ . '/../config/db.php';
-  $database = new Database();
-  try{
-      $connection = $database->connect();
-      if($connection){
-        echo 'Connected to the database';
-      }
-  }
-  catch(Exception $e){
-    echo 'Caught exception: ', $e->getMessage(), "\n";
-  }
-?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Store</title>
+  </head>
+  <body>
+    <?php
+      include __DIR__ . '/../config/db.php';
+      include __DIR__ . '/../src/layout/header.php';
+      include __DIR__ . '/../src/pages/home.php';
+      include __DIR__ . '/../src/layout/footer.php';
+    ?>
+  </body>
+</html>
