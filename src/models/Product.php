9708205b6ca77643c4664c318c09275e939bc2fb
Amr Tamer Elharery <amrelharery@gmail.com>
2025-05-14 23:16:32 +0300

    Support search

--- src/models/Product.php
+++ src/models/Product.php
@@ -1,41 +1,55 @@
 <?php
 
-  class Product {
-      private $connection;
-      private $table = 'products';
+class Product {
+    private $connection;
+    private $table = 'products';
 
-      public function __construct($connection) {
-          $this->connection = $connection;
-      }
+    public function __construct($connection) {
+        $this->connection = $connection;
+    }
 
-      public function getAllProducts($limit = 0) {
-          if ($limit > 0) {
-              $query = "SELECT * FROM " . $this->table . " LIMIT ?";
-              $stmt = $this->connection->prepare($query);
-              $stmt->bind_param("i", $limit);
-              $stmt->execute();
-              $result = $stmt->get_result();
-          } else {
-              $query = "SELECT * FROM " . $this->table;
-              $result = $this->connection->query($query);
-          }
-          $products = [];
-          while ($row = $result->fetch_assoc()) {
-              $products[] = $row;
-          }
-          
-          return $products;
-      }
+    public function getAllProducts($limit = 0) {
+        if ($limit > 0) {
+            $query = "SELECT * FROM " . $this->table . " LIMIT ?";
+            $stmt = $this->connection->prepare($query);
+            $stmt->bind_param("i", $limit);
+            $stmt->execute();
+            $result = $stmt->get_result();
+        } else {
+            $query = "SELECT * FROM " . $this->table;
+            $result = $this->connection->query($query);
+        }
+        $products = [];
+        while ($row = $result->fetch_assoc()) {
+            $products[] = $row;
+        }
+        
+        return $products;
+    }
 
-      public function getProductById($id) {
-          $query = "SELECT * FROM " . $this->table . " WHERE id = ?";
-          $stmt = $this->connection->prepare($query);
-          $stmt->bind_param("i", $id);
-          $stmt->execute();
-          
-          $result = $stmt->get_result();
-          return $result->fetch_assoc();
-      }
-  }
+    public function getProductById($id) {
+        $query = "SELECT * FROM " . $this->table . " WHERE id = ?";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bind_param("i", $id);
+        $stmt->execute();
+        
+        $result = $stmt->get_result();
+        return $result->fetch_assoc();
+    }
+    public function searchProducts($query) {
+        $query = "%" . $query . "%";
+        $stmt = $this->connection->prepare("SELECT * FROM " . $this->table . " WHERE name LIKE ? or description LIKE ?");
+        $stmt->bind_param("ss", $query, $query);
+        $stmt->execute();
+        
+        $result = $stmt->get_result();
+        $products = [];
+        while ($row = $result->fetch_assoc()) {
+            $products[] = $row;
+        }
+        
+        return $products;
+    }
+}
 
 ?>
